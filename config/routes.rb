154756290c6d8f5fe882b382f663ebbd93ece67b
EC2 Default User <ec2-user@ip-172-31-91-110.ec2.internal>
2020-11-07 06:03:39 +0000

    [Add]商品ページ作成（一覧・新商品・詳細・編集）

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,29 @@
 Rails.application.routes.draw do
-  root "genres#index"
+  root "managers/items#show"
   
+  # managers
   devise_for :managers, controllers: {
     sessions:      'managers/sessions',
     passwords:     'managers/passwords',
     registrations: 'managers/registrations'
   }
   
-  devise_for :customers
-  
-  resources :managers
-  resources :genres
+  resources :managers, only: [:index]
   
   namespace :managers do
-    resources :items, :orders, :order_items
+    resources :genres, :items, :customers, :orders, :order_items
   end
+  # /managers
+  
+  
+  
+  # customers
+  devise_for :customers
+  
+  scope module: :customers do
+    resources :items, only: [:index, :show]
+  end
+  # /customers
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
