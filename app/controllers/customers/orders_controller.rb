48727ed52d4368f22a8ae992b6e5e5c1eb248c3b
EC2 Default User <ec2-user@ip-172-31-91-110.ec2.internal>
2020-11-30 13:08:59 +0000

    [Update]注文確定

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -42,19 +42,17 @@ class Customers::OrdersController < ApplicationController
     
     @order.save
     if @order.save
+      @cart_items = CartItem.all
       @order_item = OrderItem.new
       @order_item.order_id = @order.id
-      @order_item.item_id = @cart_item.item.id
-      @order_item.price = @order.total_price
-      @order_item.amount = 0
       @cart_items.each do |cart_item|
-        @order_item.amount += cart_item.amount
+        @order_item.item_id = cart_item.item.id
+        @order_item.price = cart_item.item.price
+        @order_item.amount = cart_item.amount
+        cart_item.destroy
       end
+      @order_item.making_status = 0
       @order_item.save
-    end
-    
-    if @order_item.save
-      @cart_item.destroy
     end
     
     redirect_to thanks_orders_path
