8c368f6ded00eb2c22bc728a860553c4bf432837
EC2 Default User <ec2-user@ip-172-31-91-110.ec2.internal>
2020-12-07 16:05:51 +0000

    [Update]注文ステータスと製作ステータスを連動

--- app/controllers/managers/orders_controller.rb
+++ app/controllers/managers/orders_controller.rb
@@ -1,19 +1,27 @@
 class Managers::OrdersController < ApplicationController
   def index
     @orders = Order.order("id DESC").page(params[:page]).per(10)
-    @order_items = OrderItem.all
+    @order_item = OrderItem.all
   end
   
   def show
     @orders = Order.all
     @order = Order.find(params[:id])
     
-    @order_items = OrderItem.all
+    @order_item = OrderItem.all
   end
   
   def update
     @order = Order.find(params[:id])
     @order.update(managers_order_params)
+    if @order.progress_status == "入金確認"
+      @order_items = OrderItem.all
+      @order_items.each do |order_item|
+        if @order.id == order_item.order_id
+          order_item.update(making_status: "製作待ち")
+        end
+      end
+    end
     redirect_to managers_order_path(@order)
   end
   
